0292c899f327b0d91805e558b39beb8ce48f7709
jsalcedollanos <jhonny_salcedo21@hotmail.com>
2021-09-15 07:03:11 -0500

    Modificacion del controlador de validacion login

--- app/Http/Livewire/FormLogin.php
+++ app/Http/Livewire/FormLogin.php
@@ -6,10 +6,12 @@ use Livewire\Component;
 
 class FormLogin extends Component
 {
+    public $password;
+    public $email;
     protected $rules = [
         'email' => 'required|email|min:3|max:35',
         'password' => 'required|min:8',
-        'password2' => 'required|min:8',
+        
     ];
     public function updated($propertyName)
     {
